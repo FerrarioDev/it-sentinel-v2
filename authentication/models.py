dd4b7c79e8931ed6754865b5c3121df6ed1bceb9
Geronimo/Denso <ferrario.dev@gmail.com>
2023-11-30 10:43:10 -0300

    fix(customUser): user model now works :)

--- authentication/models.py
+++ authentication/models.py
@@ -1,8 +1,24 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
-    dnarId  = models.CharField(unique=True, max_length=9)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, dnarId, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(dnarId=dnarId, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, dnarId, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(dnarId, email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    dnarId = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -11,21 +27,13 @@ class CustomUser(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    USERNAME_FIELD = 'dnarId' 
+    area = models.CharField(max_length=255, blank=True, null=True)
 
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='customuser_groups',
-        blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-        verbose_name='groups',
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='customuser_user_permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        verbose_name='user permissions',
-    )
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'dnarId'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
     def __str__(self):
-      return "{}".format(self.dnarId)
+        return self.dnarId
--- mgmtsystem/settings.py
+++ mgmtsystem/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-# AUTH_USER_MODEL = 'authentication.CustomUser'
+AUTH_USER_MODEL = 'authentication.CustomUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
