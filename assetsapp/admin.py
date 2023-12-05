8cd5ef8d7f0cc18d9b6b40a5aaaedd2a012d0ac7
Geronimo/Denso <ferrario.dev@gmail.com>
2023-12-05 11:33:11 -0300

    feat(dbdiagram)

--- assetsapp/admin.py
+++ assetsapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Asset, Assignment, Computer, AssetCategory
 # Register your models here.
+admin.site.register(Asset)
+admin.site.register(AssetCategory)
+admin.site.register(Computer)
+admin.site.register(Assignment)
