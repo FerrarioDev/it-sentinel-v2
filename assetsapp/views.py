cc2feb3ce7dbc20d4c8399f9996b4b7b7eb40ce7
Geronimo/Denso <ferrario.dev@gmail.com>
2023-12-26 11:12:24 -0300

    feat(add_asset): creation form woking, needs improvements

--- assetsapp/views.py
+++ assetsapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.contrib import messages
 from django.views import View
 from django.views.generic import ListView
+from django.views.decorators.csrf import csrf_exempt
 from .forms import AssetCreationForm
 from .models import Asset, Computer, Assignment
 
@@ -11,35 +13,50 @@ from .models import Asset, Computer, Assignment
 def index(request):
     return render(request, 'assetsapp/index.html')
 
+
 class Add_assets(View):
-    
+    @csrf_exempt
     def get(self, request):
         form = AssetCreationForm(initial={'asset_number': '', 'description': '', 'serial_number': '', 'location': '', 'purchase_date': '', 'warranty_expiry_date': ''})
         return render(request, 'assetsapp/add_asset.html', {'form': form})
-
+    
+    @csrf_exempt
     def post(self, request):
-        form = AssetCreationForm(request.POST, initial={'asset_number': '', 'description': '', 'serial_number': '', 'location': '', 'purchase_date': '', 'warranty_expiry_date': ''})
+        form = AssetCreationForm(request.POST)
         if form.is_valid():
+            asset_id = form.cleaned_data['asset_number']
+            category = form.cleaned_data['asset_category']
+            serial_number = form.cleaned_data['serial_number']
+            
+            if Asset.objects.filter(asset_number = asset_id).exists():
+                messages.error(request, 'Asset ID is already registered')
+                return render(request, 'assetsapp/add_asset.html', {'form': form})
+    
+            if Asset.objects.filter(serial_number=serial_number).exists():
+                messages.error(request, 'Serial Number is already registered')
+                return render(request, 'assetsapp/add_asset.html', {'form': form})
+
             asset = form.save(commit=False)
 
             # Optionally create a Computer object
-            if form.cleaned_data.get('is_computer'):
+            if category.name == 'Computer':
                 computer = Computer(computer_id=form.cleaned_data['computer_id'], drive_serialnumber=form.cleaned_data['drive_serialnumber'])
                 computer.save()
                 asset.computer = computer  # Associate the Computer object with the Asset
 
             asset.save()
 
-            # Optionally create an Assignment object
+            ''' Optionally create an Assignment object
             if form.cleaned_data.get('assign_to_user'):
                 assignment = Assignment(asset=asset, assigned_to=form.cleaned_data['assigned_to'], assigned_date=form.cleaned_data['assigned_date'])
                 assignment.save()
-
+            '''
             # Redirect to the asset_detail view
-            return redirect('asset_detail', asset_number=asset.asset_number)
+            return redirect('asset_list')
+            # return redirect('asset_detail', asset_number=asset.asset_number)
 
         # If the form is invalid, render the form page with errors
-        print('not created')
+        print(form.errors)
         return render(request, 'assetsapp/add_asset.html', {'form': form})
 
 class AssetListView(ListView):
@@ -49,3 +66,4 @@ class AssetListView(ListView):
 
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
+    
